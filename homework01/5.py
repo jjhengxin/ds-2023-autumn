fd9087d343de365c9fdb227f24464f6c9947efab
jjhengxin <144665728+jjhengxin@users.noreply.github.com>
2023-12-02 16:21:39 +0800

    Delete homework01 directory

--- homework01/5.py
+++
@@ -1,2 +0,0 @@
-numbers = sorted([int(input()) for _ in range(3)])
-for i in range(3): print(numbers[i],end=' ')
--- homework01/6.py
+++
@@ -1,2 +0,0 @@
-numbers = sorted([int(input()) for _ in range(4)],reverse=True)
-for i in range(4): print(numbers[i],end=' ')
